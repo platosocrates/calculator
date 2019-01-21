2c2b2a3cda12b09b5fb221a784cbb0043e794ff4
Lee <liam.eakins@outlook.com>
2019-01-21 03:29:08 -0600

    Add most of functionality

--- js/main.js
+++ js/main.js
@@ -1,15 +1,51 @@
 // Declares constant variables
 
 const numberButtons = document.querySelectorAll('.numbers');
+
 const operatorButtons = document.querySelectorAll('.highlighted');
+
 const display = document.querySelector('#display');
+
 const clear = document.querySelector('input[name="clear"]');
+
+
+// Declares regular expressions
+
+    // checks if there is a divided by zero regex
+
+    const divZeroError = /\/0/;
+    
+    // checks if there are multiple operators in a row
+
+    const opError = /[.*/+-][.*/+-]/;
+
+    // checks if the first entered character is an operator
+
+    const startError = /^[*/+-]/;
+
+let checkForRegex = function() {
+    if (divZeroError.test(display.textContent) == true) {
+        alert("You can't divide by zero");
+        return true;
+        } else if (opError.test(display.textContent) == true) {
+        alert("You can't use two operators in a row");
+        return true;
+        } else if (startError.test(display.textContent) == true) {
+        alert("You can't start with an operator");
+        return true;
+        } else {
+        return false;
+    }
+}
+
+// trims display text if triggered by regex
+const trimDisplayText = function() {
+    display.textContent = display.textContent.slice(0, -1);
+} 
 
 // Declares changing variables
 
 let storedValue;
-
-let dataArray = [];
 
 // Clears the display.textContent on clear click
 
@@ -34,6 +70,7 @@ numberButtons.forEach((button) => {
         else {
             storedValue = button.value;
             display.textContent += storedValue;
+            if (checkForRegex() == true) { trimDisplayText();}
         }
     });
 });
@@ -47,10 +84,14 @@ operatorButtons.forEach((operator) => {
         }
         else {
             switch (operator.value) {
-                case "/": display.textContent += "/"; break;
-                case "*": display.textContent += "*"; break;
-                case "-": display.textContent += "-"; break;
-                case "+": display.textContent += "+"; break;
+                case "/" : display.textContent += "/"; 
+                if (checkForRegex() == true) { trimDisplayText();} break;
+                case "*": display.textContent += "*";
+                if (checkForRegex() == true) { trimDisplayText();} break;
+                case "-": display.textContent += "-";
+                if (checkForRegex() == true) { trimDisplayText();} break;
+                case "+": display.textContent += "+";
+                if (checkForRegex() == true) { trimDisplayText();} break;
             }
         }
     });
